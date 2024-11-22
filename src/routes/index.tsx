b439293ec736696b1abc386fa053f4fced7c8504
pedro <pfaria.sjp@ecori.com.br>
2024-11-22 14:02:36 -0300

    Criando rotas privadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { AuthRoutes } from './auth.routes'
 import { gluestackUIConfig } from "../../config/gluestack-ui.config"
 import { Box } from '@gluestack-ui/themed'
+import { AppRoutes } from './app.routes'
 
 export function Routes() {
 
@@ -12,7 +13,7 @@ export function Routes() {
     return (
         <Box flex={1} bg='$gray700'>
             <NavigationContainer theme={theme}>
-                <AuthRoutes />
+                <AppRoutes />
             </NavigationContainer>
         </Box>
     )
