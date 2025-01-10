01a9a805795d3e24d0e5ebebb03a4ee0b23d8c0e
pedro <pfaria.sjp@ecori.com.br>
2025-01-10 13:49:16 -0300

    finalizando fluxo de realizar requisição caso falhe por token expirado

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -20,86 +20,6 @@ const api = axios.create({
 let failedQueue: Array<PromiseType> = [];
 let isRefresh = false;
 
-api.registerIntercepTokenMenager = (singOut) => {
-  const interceptTokenManager = api.interceptors.response.use(
-    (response) => response,
-    async (requestError) => {
-      if (requestError.response?.status === 401) {
-        if (
-          requestError.response.data?.message === "token.expired" ||
-          requestError.response.data?.message === "token.invalid"
-        ) {
-          const { refresh_token, ...session } = await storageSession();
-
-          if (!refresh_token) {
-            console.warn("saindo...");
-            singOut();
-            return Promise.reject(requestError);
-          }
-
-          const originalRequestConfig = requestError.config;
-
-          if (isRefresh) {
-            return new Promise((resolve, reject) => {
-              failedQueue.push({
-                onSuccess: (token: string) => {
-                  originalRequestConfig.headers = {
-                    Authorization: `Bearer ${token}`,
-                  };
-                  resolve(api(originalRequestConfig));
-                },
-                onFailure: (error: AxiosError) => {
-                  reject(error);
-                },
-              });
-            });
-          }
-
-          isRefresh = true;
-
-          return new Promise(async (resolve, reject) => {
-            try {
-              const { data } = await api.post("/sessions/refresh-token", {
-                token: refresh_token,
-              });
-
-              await storageUser({
-                ...session,
-                refresh_token: data.refresh_token,
-                token: data.token,
-              });
-
-              console.log("aqui");
-              console.log(data);
-            } catch (error: any) {
-              failedQueue.forEach((request) => {
-                request.onFailure(error);
-              });
-
-              singOut();
-              reject(error);
-            } finally {
-              isRefresh = false;
-              failedQueue = [];
-            }
-          });
-        }
-        singOut();
-      }
-
-      if (requestError.response && requestError.response.data) {
-        return Promise.reject(new AppError(requestError.response.data.message));
-      } else {
-        return Promise.reject(requestError);
-      }
-    }
-  );
-
-  return () => {
-    api.interceptors.response.eject(interceptTokenManager);
-  };
-};
-
 // api.registerIntercepTokenMenager = (singOut) => {
 //   const interceptTokenManager = api.interceptors.response.use(
 //     (response) => response,
@@ -112,6 +32,7 @@ api.registerIntercepTokenMenager = (singOut) => {
 //           const { refresh_token, ...session } = await storageSession();
 
 //           if (!refresh_token) {
+//             console.warn("saindo...");
 //             singOut();
 //             return Promise.reject(requestError);
 //           }
@@ -139,18 +60,22 @@ api.registerIntercepTokenMenager = (singOut) => {
 //           return new Promise(async (resolve, reject) => {
 //             try {
 //               const { data } = await api.post("/sessions/refresh-token", {
-//                 refresh_token,
+//                 token: refresh_token,
 //               });
-//               console.log(data);
+
 //               await storageUser({
 //                 ...session,
 //                 refresh_token: data.refresh_token,
 //                 token: data.token,
 //               });
+
+//               console.log("aqui");
+//               console.log(data);
 //             } catch (error: any) {
 //               failedQueue.forEach((request) => {
 //                 request.onFailure(error);
 //               });
+
 //               singOut();
 //               reject(error);
 //             } finally {
@@ -159,7 +84,6 @@ api.registerIntercepTokenMenager = (singOut) => {
 //             }
 //           });
 //         }
-
 //         singOut();
 //       }
 
@@ -175,5 +99,104 @@ api.registerIntercepTokenMenager = (singOut) => {
 //     api.interceptors.response.eject(interceptTokenManager);
 //   };
 // };
+
+api.registerIntercepTokenMenager = (singOut) => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response,
+    async (requestError) => {
+      if (requestError.response?.status === 401) {
+        if (
+          requestError.response.data?.message === "token.expired" ||
+          requestError.response.data?.message === "token.invalid"
+        ) {
+          const { refresh_token, ...session } = await storageSession();
+
+          if (!refresh_token) {
+            singOut();
+            return Promise.reject(requestError);
+          }
+
+          const originalRequestConfig = requestError.config;
+
+          if (isRefresh) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({
+                onSuccess: (token: string) => {
+                  originalRequestConfig.headers = {
+                    Authorization: `Bearer ${token}`,
+                  };
+                  resolve(api(originalRequestConfig));
+                },
+                onFailure: (error: AxiosError) => {
+                  reject(error);
+                },
+              });
+            });
+          }
+
+          isRefresh = true;
+
+          return new Promise(async (resolve, reject) => {
+            try {
+              const { data } = await api.post("/sessions/refresh-token", {
+                refresh_token,
+              });
+
+              // console.log(data);
+
+              await storageUser({
+                ...session,
+                refresh_token: data.refresh_token,
+                token: data.token,
+              });
+
+              if (originalRequestConfig.data) {
+                originalRequestConfig.data = JSON.parse(
+                  originalRequestConfig.data
+                );
+              }
+
+              originalRequestConfig.headers = {
+                Authorization: `Bearer ${data.token}`,
+              };
+              api.defaults.headers.common[
+                "Authorization"
+              ] = `Bearer ${data.token}`;
+
+              failedQueue.forEach((request) => {
+                request.onSuccess(data.token);
+              });
+
+              console.log("token autorizado");
+
+              resolve(api(originalRequestConfig));
+            } catch (error: any) {
+              failedQueue.forEach((request) => {
+                request.onFailure(error);
+              });
+              singOut();
+              reject(error);
+            } finally {
+              isRefresh = false;
+              failedQueue = [];
+            }
+          });
+        }
+
+        singOut();
+      }
+
+      if (requestError.response && requestError.response.data) {
+        return Promise.reject(new AppError(requestError.response.data.message));
+      } else {
+        return Promise.reject(requestError);
+      }
+    }
+  );
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
+  };
+};
 
 export { api };
